bde5b672f76b57245ded4686c044fdb8909298f5
Polycarp <ghost@localhost.localdomain>
2018-11-17 11:55:25 +0300

    firt commit

---
+++ depre.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+import matplotlib.pyplab as plt
+import pandas as pd 
+import seeborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn import datasets
+%matplotlib inline
+
+#importing dataset and extracting independent variable
+data_test = pd.read_csv(test.csv)
+
+
+def 
